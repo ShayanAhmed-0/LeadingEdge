d553ce52b14f8f5584e188b17332e86e76d0e62f
Shayan Ahmed <shayanlost1@gmail.com>
2023-09-17 19:36:04 +0500

    prod

--- src/app/page.js
+++ src/app/page.js
@@ -11,7 +11,7 @@ import My_Anni_Cards from './components/cards/anni_card'
 export default function Home() {
   return (
    <>
-   {/* <My_Hero_Section/> */}
+   <My_Hero_Section/>
    {/* <div className='grid grid-cols-1 gap-10 md:grid-cols-2 lg:grid-cols-3'> 
    <My_Anni_Cards/>
    <My_Anni_Cards/>
