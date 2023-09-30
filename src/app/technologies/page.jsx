1b91d4f069d4b7da0fe34af1bc1022ca2da0e952
ShayanAhmed-0 <shayanlost1@gmail.com>
2023-09-30 16:29:50 +0500

    technologiesproduction

--- src/app/technologies/page.jsx
+++ src/app/technologies/page.jsx
@@ -1,5 +1,5 @@
 import PricingCard from '@/components/PricingCard'
-import Anim7 from '@/components/anim7/anim7'
+import Anim7 from '@/components/anim7/Anim7'
 import React from 'react'
 
 const page = () => {
