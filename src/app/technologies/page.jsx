ce9c234479f2722ed6eba8b87724bffcc43743de
ShayanAhmed-0 <shayanlost1@gmail.com>
2023-09-30 16:32:25 +0500

    technologiesnavfoo

--- src/app/technologies/page.jsx
+++ src/app/technologies/page.jsx
@@ -1,11 +1,15 @@
 import PricingCard from '@/components/PricingCard'
 import Anim7 from '@/components/anim7/Anim7'
+import Footer from '@/layout/footer/Footer'
+import Navbar from '@/layout/navbar/Navbar'
 import React from 'react'
 
 const page = () => {
   return (
     <div>
+      <Navbar/>
       <Anim7/>
+      <Footer/>
     </div>
   )
 }
