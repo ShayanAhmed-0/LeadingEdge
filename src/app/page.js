c4026de3cae4d7293bcbc24b106c54348e9b1d83
WajidAhmed-25 <wajid.g20504@iqra.edu.pk>
2023-10-01 15:35:11 +0500

    main_page

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,16 @@
 import My_Hero_Section from "@/components/My_Hero_Section";
 import Footer from "@/layout/footer/Footer";
 import Navbar from "@/layout/navbar/Navbar";
+import Mrx from "@/components/my_view/mrx";
 
 export default function Home() {
   return (
     <div className="bg-white">
-      <Navbar />
-      <My_Hero_Section />
-      <Footer />
+      {/* <Navbar />
+     <My_Hero_Section /> 
+      <Footer /> */}
+
+      <Mrx/>
     </div>
   );
 }
---
+++ src/components/my_view/mrx.jsx
@@ -0,0 +1,34 @@
+import './mrx.css'
+
+
+export default function Mrx(){
+    return(
+ 
+
+<div className='main_'> 
+
+
+<div class="bg"></div>
+<div class="bg bg2"></div>
+<div class="bg bg3"></div>
+<div class="content">
+  {/* <h1>Sliding Diagonals Background Effect</h1> */}
+
+  <h1 id="h1a" class="home-title">
+  <span className='sp1'>Leading Edge</span>
+  <span className='sp2'>Introducing Digital Creativity</span>
+</h1>
+
+<span className='span1'><a href="#"></a></span>
+
+
+  
+</div>
+
+
+
+
+</div>
+
+    )
+}
